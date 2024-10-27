c4a00ade2ea9ad635681758fa54059b6de511c8f
Mattstir <stirmayr@gmail.com>
2024-10-27 15:03:22 +0100

    Basic App shell

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import './App.css';
+import LedMapper from './components/LedMapper';
+import githubMarker from "./svg/github-mark-white.svg";
 
 function App() {
   return (
     <div className="App">
-       React test App
+        <div className="Header">
+            <div className="toolName">WLEDMapper</div>
+            <a href="https://github.com/Mattstir/WLEDMapper">
+                <img className="githubMarker" src={githubMarker} alt="Go to repository" />
+            </a>
+        </div>
+        <LedMapper/>
+        <div className="Footer">
+            Made with Love for WLED by
+            <a href="https://github.com/Mattstir">Mattstir</a>
+        </div>
     </div>
   );
 }
---
+++ src/components/LedMapper.tsx
@@ -0,0 +1,11 @@
+import "./LedMapper.css";
+
+function LedMapper() {
+    return (
+      <div className="mapper">
+          WIP - This tool is under active development and is not inteded to be used yet
+      </div>
+    );
+  }
+  
+  export default LedMapper;
